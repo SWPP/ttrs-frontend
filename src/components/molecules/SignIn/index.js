69641c16d05f50d6adc2c05ff8ca841777f33962
Tommybro <tjdwo5313@naver.com>
2018-05-02 20:21:27 +0900

    sign in no error

--- src/components/molecules/SignIn/index.js
+++ src/components/molecules/SignIn/index.js
@@ -4,8 +4,12 @@ import Button from '../../atoms/Button'
 export const SignIn = ({ onSignIn, isSignedIn }) => {
   let inputUsername
   let inputPassword
-  const onSubmit = () => {
-    onSignIn(inputUsername.value, inputPassword.value)
+  const onSubmitSignIn = () => {
+    if (inputUsername.value.trim() && inputPassword.value.trim()) {
+      onSignIn(inputUsername.value, inputPassword.value)
+      inputUsername.value = ''
+      inputPassword.value = ''
+    }
   }
 
   if (!isSignedIn) {
@@ -13,7 +17,8 @@ export const SignIn = ({ onSignIn, isSignedIn }) => {
       <div>
         <input ref={node => { inputUsername = node }} placeholder={'username'} />
         <input ref={node => { inputPassword = node }} placeholder={'password'} />
-        <Button type="submit" onClick={onSubmit}>Sign In</Button>
+        <Button type="submit" onClick={onSubmitSignIn}>Sign In</Button>
+        {/*<Button type="submit" onClick={onSubmitSignIn}>Sign Up</Button>*/}
       </div>
     )
   }
