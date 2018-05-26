9d6895f60df61a0ba96e7a7a5445c0da43f203d1
lameinthebox <lameinthebox@snu.ac.kr>
2018-05-27 05:20:21 +0900

    temp commit before pull

--- src/components/organisms/RecommendTab/index.js
+++ src/components/organisms/RecommendTab/index.js
@@ -26,12 +26,12 @@ const RecommendTab = ({ isMainPage, currentTab, myTimeTable, onUpdateMyTimeTable
     return (
       <div>
         <SearchLecture
-          onAddLecture={(newLectureId) => onUpdateMyTimeTable(myTimeTable.id, {lectures: getLectureIds()}, newLectureId)} />
+          onAddLecture={(newLectureId) => onUpdateMyTimeTable(myTimeTable.id, { lectures: getLectureIds() }, newLectureId)} />
         <hr />
         <TimeTable
-          onModifyMemo={(memo) => onUpdateMyTimeTable(myTimeTable.id, {memo}, null)}
-          onModifyTitle={(title) => onUpdateMyTimeTable(myTimeTable.id, {title}, null)}
-          onDeleteLecture={(lectureId) => onUpdateMyTimeTable(myTimeTable.id, {lectures: getLectureIdsWithout(lectureId)}, -lectureId )}
+          onModifyMemo={(memo) => onUpdateMyTimeTable(myTimeTable.id, { memo }, null)}
+          onModifyTitle={(title) => onUpdateMyTimeTable(myTimeTable.id, { title }, null)}
+          onDeleteLecture={(lectureId) => onUpdateMyTimeTable(myTimeTable.id, { lectures: getLectureIdsWithout(lectureId) }, -lectureId )}
           {...myTimeTable}
         />
       </div>
