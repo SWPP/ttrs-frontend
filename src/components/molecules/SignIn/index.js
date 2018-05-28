44bfcc4c4d535d77fd48cd82b6f259979d48502a
holenet <holenet6147@naver.com>
2018-05-29 03:11:48 +0900

    semantic UI setup & SignIn design

--- src/components/molecules/SignIn/index.js
+++ src/components/molecules/SignIn/index.js
@@ -1,30 +1,86 @@
 import React from 'react'
-import Button from '../../atoms/Button'
+import PropTypes from 'prop-types'
+import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
 
-const SignIn = ({ onSignIn, onGoSignUpPage, isMainPage, isSignUpPage }) => {
-  let inputUsername
-  let inputPassword
-  const onSubmitSignIn = () => {
-    if (inputUsername.value.trim() && inputPassword.value.trim()) {
-      onSignIn(inputUsername.value, inputPassword.value)
-      inputUsername.value = ''
-      inputPassword.value = ''
+class SignIn extends React.Component {
+  state = {}
+
+  handleChange = (e, { name, value }) => {
+    this.setState({ [name]: value })
+  }
+
+  handleSubmitSignIn = () => {
+    if (this.state.username.trim() && this.state.password.trim()) {
+      this.props.onSignIn(this.state.username, this.state.password)
+      this.setState({ username: '', password: '' })
     } else {
       console.log('blank input not allowed')
     }
   }
 
-  if (!isMainPage && !isSignUpPage) {
-    return (
-      <div>
-        <input ref={node => { inputUsername = node }} placeholder={'username'} /> <br />
-        <input ref={node => { inputPassword = node }} placeholder={'password'} type="password" /> <br />
-        <Button type="submit" onClick={onSubmitSignIn}>Sign In</Button> <br />
-        <Button type="submit" onClick={onGoSignUpPage}>Sign Up</Button>
-      </div>
-    )
+  render() {
+    const { username, password } = this.state
+
+    if (!this.props.isMainPage && !this.props.isSignUpPage) {
+      return (
+        <div className="sign-in-form">
+          <style>{`
+          body > main,
+          body > main > div,
+          body > main > div > div.sign-in-form {
+            height: 100%;
+          }
+        `}</style>
+          <Grid
+            textAlign="center"
+            style={{ height: '100%' }}
+            verticalAlign="middle"
+          >
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <Header as="h1" color="teal" textAlign="center">
+                TTRS
+              </Header>
+              <Form size="large">
+                <Segment stacked>
+                  <Form.Input
+                    fluid
+                    icon="user"
+                    iconPosition="left"
+                    placeholder="Username"
+                    name="username"
+                    value={username}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Input
+                    fluid
+                    icon="lock"
+                    iconPosition="left"
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={this.handleChange}
+                  />
+                  <Button.Group widths="2">
+                    <Button color="teal" size="large" onClick={this.props.onGoSignUpPage}>Sign Up</Button>
+                    <Button color="teal" size="large" onClick={this.handleSubmitSignIn}>Sign In</Button>
+                  </Button.Group>
+                </Segment>
+              </Form>
+            </Grid.Column>
+          </Grid>
+        </div>
+      )
+    }
+    return null
   }
-  return null
+}
+
+SignIn.propTypes = {
+  onSignIn: PropTypes.func,
+  onGoSignUpPage: PropTypes.func,
+  isMainPage: PropTypes.bool,
+  isSignUpPage: PropTypes.bool,
 }
 
 export default SignIn
