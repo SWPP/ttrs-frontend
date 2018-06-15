d1ec33e767d68f0563fb7f09fd99ee90cde52cb6
Tommybro <tjdwo5313@naver.com>
2018-06-16 01:56:04 +0900

    implemented recommend options

--- src/components/organisms/RecommendTab/index.js
+++ src/components/organisms/RecommendTab/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, Divider } from 'semantic-ui-react'
+import { Divider, Form, Grid, Progress } from 'semantic-ui-react'
 import TimeTable from '../../../containers/TimeTable'
 
 export const getLectureIds = (timeTable) => {
@@ -24,7 +24,23 @@ export const getLectureIdsWithout = (lectureId, timeTable) => {
 let inputRecommendedTimeTableIndex = { value: 0 }
 
 class RecommendTab extends React.Component {
+  state = {
+    avoidSuccessiveLecture: false,
+    avoidVoidLecture: false,
+    avoidFirstLecture: false,
+    jeonpilWeight: '250',
+    jeonseonWeight: '250',
+    gyoyangWeight: '250',
+    creditUserWant: '15',
+  }
+
+  handleChange = (e, { name, value }) => {
+    this.setState({ [name]: value })
+  }
+
   render() {
+    const weightSum = (this.state.jeonpilWeight * 1) + (this.state.jeonseonWeight * 1) + (this.state.gyoyangWeight * 1)
+
     return (
       <div>
         <h1>My TimeTable</h1>
@@ -42,12 +58,124 @@ class RecommendTab extends React.Component {
         />
         <Divider />
         <h1>Recommended TimeTable</h1>
-        <Button
-          color="teal"
-          content="Recommend"
-          loading={false}
-          onClick={this.props.onGetRecommendation}
-        /> <br />
+        <Form>
+          <Grid>
+            <Grid.Row columns={5}>
+              <Grid.Column>
+                <Form.Radio
+                  label="연강 회피"
+                  toggle
+                  onChange={() => this.setState({ avoidSuccessiveLecture: !this.state.avoidSuccessiveLecture })}
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Form.Radio
+                  label="공강 회피"
+                  toggle
+                  onChange={() => this.setState({ avoidVoidLecture: !this.state.avoidVoidLecture })}
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Form.Radio
+                  label="1교시 회피"
+                  toggle
+                  onChange={() => this.setState({ avoidFirstLecture: !this.state.avoidFirstLecture })}
+                />
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row columns={4}>
+              <Grid.Column>
+                <Progress
+                  label="전필 가중치"
+                  value={this.state.jeonpilWeight}
+                  total={weightSum}
+                  percent={Math.round((this.state.jeonpilWeight * 100) / weightSum)}
+                  progress
+                  color="orange"
+                />
+                <Form.Input
+                  style={{ marginTop: -10 }}
+                  type="range"
+                  name="jeonpilWeight"
+                  min={1}
+                  max={500}
+                  step={1}
+                  value={this.state.jeonpilWeight}
+                  onChange={this.handleChange}
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Progress
+                  label="전선 가중치"
+                  value={this.state.jeonseonWeight}
+                  total={weightSum}
+                  percent={Math.round((this.state.jeonseonWeight * 100) / weightSum)}
+                  progress
+                  color="blue"
+                />
+                <Form.Input
+                  style={{ marginTop: -10 }}
+                  type="range"
+                  name="jeonseonWeight"
+                  min={1}
+                  max={500}
+                  step={1}
+                  value={this.state.jeonseonWeight}
+                  onChange={this.handleChange}
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Progress
+                  label="교양 가중치"
+                  value={this.state.gyoyangWeight}
+                  total={weightSum}
+                  percent={Math.round((this.state.gyoyangWeight * 100) / weightSum)}
+                  progress
+                  color="green"
+                />
+                <Form.Input
+                  style={{ marginTop: -10 }}
+                  type="range"
+                  name="gyoyangWeight"
+                  min={1}
+                  max={500}
+                  step={1}
+                  value={this.state.gyoyangWeight}
+                  onChange={this.handleChange}
+                />
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row columns={4} style={{ marginTop: -15 }}>
+              <Grid.Column>
+                <Form.Input
+                  label={`희망 학점: ${this.state.creditUserWant}`}
+                  type="range"
+                  name="creditUserWant"
+                  min={1}
+                  max={21}
+                  step={1}
+                  value={this.state.creditUserWant}
+                  onChange={this.handleChange}
+                />
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row columns={8} style={{ marginTop: -15 }}>
+              <Grid.Column /><Grid.Column /><Grid.Column /><Grid.Column /><Grid.Column />
+              <Grid.Column>
+                <Form.Button
+                  color="teal"
+                  content="Recommend"
+                  loading={false}
+                  onClick={this.props.onGetRecommendation}
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Form>
+        <Divider />
         <select
           ref={node => { inputRecommendedTimeTableIndex = node }}
           onChange={() => this.props.onSelectRecommendedTimeTable(this.props.recommendedTimeTables[inputRecommendedTimeTableIndex.value])}
